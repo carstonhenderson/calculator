af37690853f3c2d1ffb9df344a6712676d412075
Carston Henderson <carstonhenderson@icloud.com>
2019-07-29 10:38:12 -0600

    Disable double tap zoom and button text highlighting for PWA

--- src/components/calculatorButton.js
+++ src/components/calculatorButton.js
@@ -6,7 +6,7 @@ const CalculatorButton = ({ value, color, onClick }) => (
     onClick={() => onClick(value)}
     whileHover={{ scale: 1.05 }}
     whileTap={{ scale: 0.95 }}
-    className={`bg-${color}-600 rounded mx-1 p-4 flex-1`}
+    className={`bg-${color}-600 rounded mx-1 p-4 flex-1 button`}
   >
     {value}
   </motion.button>

1450760596771228cfb4ba5bf605df45e4b95d19
Carston Henderson <carstonhenderson@icloud.com>
2019-08-07 16:45:51 -0600

    Prevent double tap zoom and copy functionality on clear button

--- src/components/clearButton.js
+++ src/components/clearButton.js
@@ -6,7 +6,7 @@ const ClearButton = ({ handleClick }) => (
     onClick={handleClick}
     whileHover={{ scale: 1.05 }}
     whileTap={{ scale: 0.95 }}
-    className="bg-gray-700 py-1 px-2 rounded-full text-xs"
+    className="bg-gray-700 py-1 px-2 rounded-full text-xs button"
   >
     Clear
   </motion.button>

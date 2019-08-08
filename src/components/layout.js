d547e554d6b876bc97faacd523df58d271d005fe
Carston Henderson <carstonhenderson@icloud.com>
2019-08-08 13:27:37 -0600

    Thanks webpack
    During build time, components are rendered on the server, where window is not defined. This fixes the build error so window is only accessed after the component has mounted.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import './layout.css'
 
-const Layout = ({ children }) => (
+const Layout = ({ children }) => {
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    setHeight(window.innerHeight)
+  })
+  
+  return (
   <main
     className="bg-black text-gray-100 flex justify-center items-center"
-    style={{ height: window.innerHeight }}
+    style={{ height: height }}
   >
     {children}
   </main>
 )
+  }
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
